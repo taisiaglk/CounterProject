11c5f7524c8674ca14051c45df16f1846007d2cb
Taisia Galkina <galkina.ttasya@gmail.com>
2023-07-09 16:22:38 +0300

    Реализована простая версия приложения

--- CounterProject/ViewController.swift
+++ CounterProject/ViewController.swift
@@ -1,19 +1,25 @@
-//
-//  ViewController.swift
-//  CounterProject
-//
-//  Created by Тася Галкина on 09.07.2023.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cntTouch: UILabel!
+    @IBOutlet weak var touchButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        cntTouch.textAlignment = .center
+        touchButton.layer.cornerRadius = touchButton.frame.size.width / 2
+        touchButton.backgroundColor = UIColor.systemGray
     }
-
-
+    
+    private var cnt : Int = 0
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        cnt += 1
+        var text : String = "Значение счётчика: \(cnt)"
+        cntTouch.text = text
+    }
+    
 }
 
